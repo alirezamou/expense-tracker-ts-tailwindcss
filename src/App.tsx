dd8c6cf5f6be4a802fee2dc97be8d2bef6062707
alirezamou <am808036@gmail.com>
2023-02-06 01:09:48 +0330

    add Balance component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from "./components/Header";
+import Balance from "./components/Balance";
 import './App.css';
 
 function App() {
-  return (
+  return (<>
         <Header title={"Expense Tracker"} />
+        <div className='my-[30px] mx-auto w-[350px]'>
+          <Balance />
+        </div>
+  </>
   );
 }
 
