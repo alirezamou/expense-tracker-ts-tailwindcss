0ef4bbbb2b5b3c0681f84ea1bedf618b7b37aba1
alirezamou <am808036@gmail.com>
2023-02-07 00:14:51 +0330

    add IncomeExpense component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpense from './components/IncomeExpense';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <Header title={"Expense Tracker"} />
         <div className='my-[30px] mx-auto w-[350px]'>
           <Balance />
+          <IncomeExpense />
         </div>
   </>
   );
---
+++ src/components/IncomeExpense.tsx
@@ -0,0 +1,14 @@
+export default function IncomeExpense () {
+    return (
+        <div className="flex gap-4 bg-white shadow p-[20px] justify-between my-[20px] mx-0">
+            <div className="flex-1 text-center border-r border-solid border-[#dedede]">
+                <h2 className="font-bold uppercase text-xl">Income</h2>
+                <p className="text-green-600 text-xl">+0.00</p>
+            </div>
+            <div className="flex-1 text-center">
+                <h2 className="font-bold uppercase text-xl">Expense</h2>
+                <p className="text-red-600 text-xl">-0.00</p>
+            </div>
+        </div>
+    );
+}
