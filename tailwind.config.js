c86a014a2ec16a955bac8005702a2d9e09270537
alirezamou <am808036@gmail.com>
2023-02-07 00:13:53 +0330

    add box shadow style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        default: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
+      },
       fontFamily: {
         sans: ["Lato", "sans-serif"],
       },
