f8c79d57e3057992554413f87a80db723b32ff95
alirezamou <am808036@gmail.com>
2023-02-11 20:09:10 +0330

    calculate balance in Balance component

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+
+import { GlobalContext } from "../context/GlobalState";
+
 export default function Balance () {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map((trx => trx.amount));
+    const total = amounts.reduce((prev, cur) => prev += cur).toFixed(2);
     return (
         <div>
             <h4 className="uppercase">Your Balance</h4>
-            <h1 className="font-bold text-3xl tracking-wider">$0.00</h1>
+            <h1 className="font-bold text-3xl tracking-wider">${total}</h1>
         </div>
     );
 }
